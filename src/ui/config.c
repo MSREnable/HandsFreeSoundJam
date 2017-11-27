331402f12ad3c5999d731f41bdf44d6b4b9f34b5
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-11-27 12:08:40 -0500

    added helpful button macros to config screen

--- src/ui/config.c
+++ src/ui/config.c
@@ -10,25 +10,33 @@
 #include "eyejam.h"
 #include "nanovg.h"
 
-static void open_presets(jam_button *but, void *ud)
-{
-    jam_ui *ui;
-    ui = ud;
-        
-    jam_ui_screen(ui, JAM_PRESETS);
+#define NBUTTONS 3
+#define BSPACING CONSTANT(50)
+#define OPENSCREEN(name, MACRO) \
+static void open_##name(jam_button *but, void *ud) \
+{\
+    jam_ui *ui;\
+    ui = ud;\
+    jam_ui_screen(ui, MACRO);\
 }
 
-static void open_launcher(jam_button *but, void *ud)
-{
-    jam_ui *ui;
-    ui = ud;
-        
-    jam_ui_screen(ui, JAM_LAUNCHER);
-}
+OPENSCREEN(presets, JAM_PRESETS);
+OPENSCREEN(launcher, JAM_LAUNCHER);
+
+typedef struct {
+    const char *name;
+    jam_button_cb cb;
+} preset_button_config;
+
+static preset_button_config bconfig[] = {
+    {"Presets", open_presets},
+    {"Loop Mode", open_launcher},
+    {"Mixer", open_launcher},
+};
 
 struct jam_config {
     jam_ui *top;
-    jam_button *presets;
+    jam_button *but[NBUTTONS];
     jam_button *launcher;
 };
 
@@ -41,19 +49,33 @@ void jam_config_init(jam_config *config, jam_ui *ui)
 {
     int centerw;
     int centerh;
+    int xpos;
+    int ypos;
+    int incr;
+    int i;
     centerw = jam_win_width() / 2;
     centerh = jam_win_height() / 2;
 
     config->top = ui;
-    config->presets = malloc(jam_button_size());
-    jam_button_init(config->presets);
-    jam_button_setsize(config->presets, CONSTANT(100), CONSTANT(100));
-    jam_button_pos(config->presets, 
-        centerw - CONSTANT(50), 
-        centerh - CONSTANT(50));
-    jam_button_cb_trigger(config->presets, open_presets);
-    jam_button_text(config->presets, "Presets");
-    jam_button_data(config->presets, config->top);
+
+    ypos = centerh - CONSTANT(50);
+    xpos = centerw - 
+        ((CONSTANT(100) * NBUTTONS) + 
+        (BSPACING * (NBUTTONS - 1))) / 2;
+
+    incr = CONSTANT(100) + BSPACING;
+
+
+    for(i = 0; i < NBUTTONS; i++) {
+        config->but[i] = malloc(jam_button_size());
+        jam_button_init(config->but[i]);
+        jam_button_setsize(config->but[i], CONSTANT(100), CONSTANT(100));
+        jam_button_pos(config->but[i], xpos, ypos);
+        jam_button_cb_trigger(config->but[i], bconfig[i].cb);
+        jam_button_text(config->but[i], bconfig[i].name);
+        jam_button_data(config->but[i], config->top);
+        xpos += incr;
+    }
     
     config->launcher= malloc(jam_button_size());
     jam_button_init(config->launcher);
@@ -68,21 +90,30 @@ void jam_config_init(jam_config *config, jam_ui *ui)
 
 void jam_config_free(jam_config *config)
 {
-    jam_button_free(config->presets);
-    free(config->presets);
+    int i;
+    for(i = 0; i < NBUTTONS; i++) {
+        jam_button_free(config->but[i]);
+        free(config->but[i]);
+    }
     jam_button_free(config->launcher);
     free(config->launcher);
 }
 
 void jam_config_interact(jam_config *config, double x, double y, double step)
 {
-    jam_button_interact(config->presets, x, y, step);
+    int i;
+    for(i = 0; i < NBUTTONS; i++) {
+        jam_button_interact(config->but[i], x, y, step);
+    }
     jam_button_interact(config->launcher, x, y, step);
 }
 
 void jam_config_draw(NVGcontext *vg, jam_config *config)
 {
-    jam_button_draw(vg, config->presets);
+    int i;
+    for(i = 0; i < NBUTTONS; i++) {
+        jam_button_draw(vg, config->but[i]);
+    }
     jam_button_draw(vg, config->launcher);
 }
 
